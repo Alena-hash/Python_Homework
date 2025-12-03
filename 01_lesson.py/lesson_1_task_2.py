7c0cc972448fd1583d72effefa99bd234906cf30
Alena1990 <atarasowa9@gmail.com>
2025-12-03 20:29:58 +0300

    доработки

--- 01_lesson.py/lesson_1_task_2.py
+++ 01_lesson.py/lesson_1_task_2.py
@@ -1,8 +1,5 @@
 my_age = 35
 print(my_age)
-<<<<<<< HEAD
 
 my_age = (38)
 print(my_age)
-=======
->>>>>>> 9a4b68a07b0136c38f430c1bfe460fedb0ba8bb0
--- 01_lesson.py/lesson_1_task_3.py
+++ 01_lesson.py/lesson_1_task_3.py
@@ -1,7 +1,3 @@
 first_name = input("Вас зовут?")
 last_name = input("Ваш возраст")
-<<<<<<< HEAD
 print(f"Вас зовут: {last_name} {first_name}")
-=======
-print("Вас зовут" + first_name + last_name)
->>>>>>> 9a4b68a07b0136c38f430c1bfe460fedb0ba8bb0
