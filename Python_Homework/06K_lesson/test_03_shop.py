68f9721e03a7c77da096aa2231f51dfa3a6892c3
Alena1990 <atarasowa9@gmail.com>
2026-01-17 22:19:33 +0300

    lesson6K

---
+++ Python_Homework/06K_lesson/test_03_shop.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def test_shop_purchase():
+
+    driver = webdriver.Firefox()
+
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+
+    items = [
+            "Sauce Labs Backpack",
+            "Sauce Labs Bolt T-Shirt",
+            "Sauce Labs Onesie"
+        ]
+
+    for item in items:
+        driver.find_element(By.XPATH, f"//div[text()='{item}']/ancestor::div[@class='inventory_item']//button").click()
+
+    driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+
+    driver.find_element(By.ID, "checkout").click()
+
+    driver.find_element(By.ID, "first-name").send_keys("Алена")
+    driver.find_element(By.ID, "last-name").send_keys("Тарасова")
+    driver.find_element(By.ID, "postal-code").send_keys("123456")
+
+    driver.find_element(By.CSS_SELECTOR, ".cart_button").click()
+
+    total_element = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "summary_total_label"))
+        )
+
+    total_text = total_element.text
+
+    assert "$58.29" in total_text, f"Итоговая сумма не равна $58.29, а равна {total_text}"
+
+    driver.quit()
+    
